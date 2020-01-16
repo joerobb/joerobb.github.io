b227331e4c87631d517d944e58dc9bfe82047e64
robb_joe <robb_joe@hotmail.co.uk>
2020-01-16 16:11:20 +0000

    Reeeeeeevamp

--- app.js
+++ app.js
@@ -1,81 +1,44 @@
-const menu = document.querySelector('.menu');
-const ul = document.querySelector('.options');
-const btn = menu.querySelector('.nav-tgl');
+var app = document.getElementById('app');
 
-//Displaying menu when toggle is clicked
+var typewriter = new Typewriter(app, {
+    loop: true
+});
 
-btn.addEventListener('click', evt => {
-  if (menu.className.indexOf('active') === -1) {
-		menu.classList.add('active');
-    ul.style.display = "block";
+typewriter.typeString('Oooooh.')
+    .pauseFor(1500)
+    .typeString(' That\'s a nice website.')
+    .pauseFor(1000)
+    .deleteChars(13)
+    .typeString(' <strong>really</strong> nice website.')
+    .pauseFor(20000)
+    .start();
 
+
+//Toggle MENU
+const toggler = document.querySelector('.menu__toggler');
+const menu    = document.querySelector('.menu');
+const overlay = document.getElementById('overlay');
+const nav     = document.querySelector('.nav');
+/*
+ * Toggles on and off the 'active' class on the menu
+ * and the toggler button.
+ */
+toggler.addEventListener('click', () => {
+  toggler.classList.toggle('active');
+  menu.classList.toggle('active');
+});
+
+nav.addEventListener('click', () => {
+  toggler.classList.toggle('active');
+  menu.classList.toggle('active');
+  overlay.style.display = "none";
+})
+
+function myFunction() {
+  var x = document.getElementById("overlay");
+  if (x.style.display === "none") {
+    x.style.display = "block";
   } else {
-		menu.classList.remove('active');
-    ul.style.display = "none";
+    x.style.display = "none";
   }
-});
-
-ul.addEventListener('click', evt => {
-    menu.classList.remove('active');
-    ul.style.display = "none";
-});
-
-
-//Typewriter
-
-var TxtType = function(el, toRotate, period) {
-        this.toRotate = toRotate;
-        this.el = el;
-        this.loopNum = 0;
-        this.period = parseInt(period, 10) || 2000;
-        this.txt = '';
-        this.tick();
-        this.isDeleting = false;
-    };
-
-    TxtType.prototype.tick = function() {
-        var i = this.loopNum % this.toRotate.length;
-        var fullTxt = this.toRotate[i];
-
-        if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-        } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-        }
-
-        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-        var that = this;
-        var delta = 200 - Math.random() * 100;
-
-        if (this.isDeleting) { delta /= 2; }
-
-        if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-        } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
-        }
-
-        setTimeout(function() {
-        that.tick();
-        }, delta);
-    };
-
-    window.onload = function() {
-        var elements = document.getElementsByClassName('typewrite');
-        for (var i=0; i<elements.length; i++) {
-            var toRotate = elements[i].getAttribute('data-type');
-            var period = elements[i].getAttribute('data-period');
-            if (toRotate) {
-              new TxtType(elements[i], JSON.parse(toRotate), period);
-            }
-        }
-        // INJECT CSS
-        var css = document.createElement("style");
-        css.type = "text/css";
-        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em #fff}";
-        document.body.appendChild(css);
-    };
+}
